0a9691fd1b4df84e5026c8f1be0a3b430bf8192a
Ryan Roden-Corrent <ryan@rcorre.net>
2020-12-01 18:00:02 -0500

    Implement placeholder auth hook

--- main.go
+++ main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
 
+	"github.com/heroiclabs/nakama-common/api"
 	"github.com/heroiclabs/nakama-common/runtime"
 )
 
 func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
-	logger.Info("module loaded")
+	if err := initializer.RegisterBeforeAuthenticateCustom(BeforeAuthItch); err != nil {
+		logger.Error("Unable to register itch auth module: %v", err)
+		return err
+	}
+	logger.Info("Itch auth module loaded")
 	return nil
+}
+
+func BeforeAuthItch(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, in *api.AuthenticateCustomRequest) (*api.AuthenticateCustomRequest, error) {
+	if in.GetAccount().GetId() == "blahblah" {
+		return in, nil
+	}
+	return in, errors.New("Bad name")
 }
