7240e0083246be38116778a958d7e4be259bde86
Ryan Roden-Corrent <ryan@rcorre.net>
2020-12-06 15:02:38 -0500

    Implement itchio auth.
    The client passes the itch JWT from the ITCHIO_API_KEY environment
    variable as the ID field of authenticate_custom. The BeforeAuthItch
    hook intercepts this and makes a call to the itch server using this
    token. If successful, the hook replaces the ID with the user's ID from
    itch. If a custom username was not provided, the username is set to the
    itch username.

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/heroiclabs/nakama-common/api"
 	"github.com/heroiclabs/nakama-common/runtime"
 )
+
+const itchURL = "https://itch.io/api/1/jwt/me"
 
 func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
 	if err := initializer.RegisterBeforeAuthenticateCustom(BeforeAuthItch); err != nil {
@@ -19,8 +25,51 @@ func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runti
 }
 
 func BeforeAuthItch(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, in *api.AuthenticateCustomRequest) (*api.AuthenticateCustomRequest, error) {
-	if in.GetAccount().GetId() == "blahblah" {
-		return in, nil
+	token := in.GetAccount().GetId()
+
+	if token == "" {
+		return in, errors.New("Missing itch.io token")
 	}
-	return in, errors.New("Bad name")
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", itchURL, nil)
+	if err != nil {
+		return in, fmt.Errorf("http.NewRequest failed: %v", err)
+	}
+
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return in, fmt.Errorf("client.Do failed: %v", err)
+	}
+	if resp.StatusCode >= 300 {
+		return in, fmt.Errorf("itch request error: %s", resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	var response struct {
+		User *struct {
+			Username    string
+			DisplayName string `json:"display_name"`
+			ID          int
+		}
+		Errors []string
+	}
+	if err := json.Unmarshal(body, &response); err != nil {
+		return in, fmt.Errorf("Failed to unmarshal itch response: %v", err)
+	}
+	if len(response.Errors) > 0 {
+		return in, fmt.Errorf("itch response has errors: %v", response.Errors)
+	}
+
+	// Add a prefix to ensure our ID is >= 6 characters (itch IDs may be shorter)
+	in.Account.Id = fmt.Sprintf("itch-%d", response.User.ID)
+
+	if in.Username == "" {
+		in.Username = response.User.Username
+	}
+
+	return in, nil
 }
